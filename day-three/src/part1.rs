5dd52f4237de61e73ac98f319d9e3db435b2efec
LapinoLapidus <LapinoLapidus@users.noreply.github.com>
2023-12-03 20:32:36 +0100

    Day 3 Part 2

--- day-three/src/part1.rs
+++ day-three/src/part1.rs
@@ -1,7 +1,5 @@
-use std::collections::HashMap;
 use std::fs;
 use std::str::FromStr;
-
 use lazy_static::lazy_static;
 use regex::Regex;
 
@@ -10,16 +8,17 @@ lazy_static! {
 }
 
 fn is_symbol(s: &String) -> bool {
-    if s == "-1" { return false; }
+    if s == "-1" {return false}
     let s = s.chars().collect::<Vec<char>>();
     let s = s.first().unwrap();
-    !(s.is_numeric() || *s == '.')
+    let ret = !(s.is_numeric() || *s == '.');
+    5+5;
+    return ret
 }
 
-fn get_adjacent(position: (isize, isize), matrix: &Vec<Vec<String>>, current_number: usize, mut adjacent_gears: &mut HashMap<(isize, isize), Vec<usize>>) -> bool {
+fn get_adjacent(position: (isize, isize), matrix: &Vec<Vec<String>>) -> bool {
     let binding = String::from("-1");
     let vec_binding = vec![binding.clone()];
-
     let curr = matrix.get(position.0 as usize).unwrap().get(position.1 as usize).unwrap();
     let left = matrix.get(position.0 as usize).unwrap_or(&vec_binding).get((position.1 - 1) as usize).unwrap_or(&binding);
     let right = matrix.get(position.0 as usize).unwrap_or(&vec_binding).get((position.1 + 1) as usize).unwrap_or(&binding);
@@ -30,32 +29,8 @@ fn get_adjacent(position: (isize, isize), matrix: &Vec<Vec<String>>, current_num
     let btm_left = matrix.get((position.0 + 1) as usize).unwrap_or(&vec_binding).get((position.1 - 1) as usize).unwrap_or(&binding);
     let btm_right = matrix.get((position.0 + 1) as usize).unwrap_or(&vec_binding).get((position.1 + 1) as usize).unwrap_or(&binding);
 
-
-    if left == "*" {
-        adjacent_gears.entry((position.0, position.1 - 1)).or_insert(vec![]).push(current_number);
-    }
-    if right == "*" {
-        adjacent_gears.entry((position.0, position.1 + 1)).or_insert(vec![]).push(current_number);
-    }
-    if top == "*" {
-        adjacent_gears.entry((position.0 - 1, position.1)).or_insert(vec![]).push(current_number);
-    }
-    if bottom == "*" {
-        adjacent_gears.entry((position.0 + 1, position.1)).or_insert(vec![]).push(current_number);
-    }
-    if top_left == "*" {
-        adjacent_gears.entry((position.0 - 1, position.1 - 1)).or_insert(vec![]).push(current_number);
-    }
-    if top_right == "*" {
-        adjacent_gears.entry((position.0 - 1, position.1 + 1)).or_insert(vec![]).push(current_number);
-    }
-    if btm_left == "*" {
-        adjacent_gears.entry((position.0 + 1, position.1 - 1)).or_insert(vec![]).push(current_number);
-    }
-    if btm_right == "*" {
-        adjacent_gears.entry((position.0 + 1, position.1 + 1)).or_insert(vec![]).push(current_number);
-    }
-
+    // println!("{}: {} {} {} {} {} {} {} {}", curr, left, right, top, bottom, top_left, top_right, btm_left, btm_right);
+    // println!("{}", is_symbol(left) || is_symbol(right) || is_symbol(top) || is_symbol(bottom) || is_symbol(top_left) || is_symbol(top_right) || is_symbol(btm_left) || is_symbol(btm_right));
     is_symbol(left) || is_symbol(right) || is_symbol(top) || is_symbol(bottom) || is_symbol(top_left) || is_symbol(top_right) || is_symbol(btm_left) || is_symbol(btm_right)
 }
 
@@ -73,24 +48,20 @@ fn main() {
     }
 
     let mut count = 0;
-    let mut gears: HashMap<(isize, isize), Vec<usize>> = HashMap::new();
+
     for (i, line) in matrix.iter().enumerate() {
         let line_str = line.join("");
         for capture in NUM_REGEX.find_iter(&line_str) {
-            if capture.len() == 0 { continue; };
+            if capture.len() == 0 {continue};
             println!("{:?}", capture);
             let mut valid = false;
             for j in (capture.start()..capture.end()) {
-                if valid { continue; }
-                valid = get_adjacent((i as isize, j as isize), &matrix, usize::from_str(capture.as_str()).unwrap(), &mut gears);
+                if valid {continue}
+                valid = get_adjacent((i as isize, j as isize), &matrix);
             }
             println!("{} is {}", capture.as_str(), valid);
-            if valid { count += i32::from_str(capture.as_str()).unwrap() }
+            if valid {count += i32::from_str(capture.as_str()).unwrap()}
         }
-    }
-    let mut count = 0;
-    for (k, v) in gears.iter() {
-        if v.len() == 2 { count += (v.first().unwrap() * v.last().unwrap()) }
     }
     println!("{:?}", count);
 }
